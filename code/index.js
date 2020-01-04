940fc7727aa5b563ef647c3cc489d3f89cc386f3
Derzhavin <derzhavind@bk.ru>
2020-01-04 15:58:23 +0300

    added new note about .js autoformating, added sprites and spriteManager

--- code/index.js
+++ code/index.js
@@ -2,10 +2,13 @@
 
 import GameManager from './managers/gameManager.js';
 import MapManager from './managers/mapManager.js';
+import SpriteManager from './managers/spriteManager.js';
+
 import map from './maps/map.js';
 
 let mapManager = new MapManager();
 let gameManager = new GameManager();
+let spriteManager = new SpriteManager();
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -14,6 +17,8 @@ function loadAll() {
     mapManager.parseMap(map); // загрузка карты
     mapManager.parseEntities(); // разбор сущностей карты
     mapManager.draw(ctx); // отобразить карту
+
+    spriteManager.loadAtlas();
 }
 
 loadAll();
---
+++ code/sprites.js
@@ -0,0 +1,37 @@
+export default {
+    "frames": {
+        "tank_sand": {
+            "frame": {
+                "x": 0,
+                "y": 0,
+                "w": 42,
+                "h": 46
+            },
+            "rotated": false,
+            "trimmed": false,
+            "spriteSourceSize": {"x": 0, "y": 0, "w": 42, "h": 46},
+            "sourceSize": {"w": 42, "h": 46}
+        },
+        "tank_red": {
+            "frame": {"x": 43, "y": 0, "w": 38, "h": 46},
+            "rotated": false,
+            "trimmed": false,
+            "spriteSourceSize": {"x": 0, "y": 0, "w": 38, "h": 46},
+            "sourceSize": {"w": 38, "h": 46}
+        },
+        "tank_bigRed_photo-resizer.ru": {
+            "frame": {"x": 0, "y": 47, "w": 42, "h": 42},
+            "rotated": false,
+            "trimmed": false,
+            "spriteSourceSize": {"x": 0, "y": 0, "w": 42, "h": 42},
+            "sourceSize": {"w": 42, "h": 42}
+        }
+    },
+    "meta": {
+        "app": "https://www.leshylabs.com/apps/sstool/",
+        "version": "Leshy SpriteSheet Tool v0.8.4",
+        "image": "../reosurces/spritesheet.png",
+        "size": {"w": 81, "h": 89},
+        "scale": 1
+    }
+}
